4536134d033dfa26e4c28bcfce4cd70067b53010
mituttta051 <amityutneva@mail.ru>
2024-06-28 12:11:24 +0300

    Additional files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/script.ts',
+    output: {
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+};
