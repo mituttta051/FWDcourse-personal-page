3df3ecf2fc8213795907048e14c57c048d0b190c
mituttta051 <mituttta051@users.noreply.github.com>
2024-07-06 20:38:47 +0000

    deploy: 8dad8e177682dd92a6cf688b8a66d5e55e5d28ee

---
+++ _next/static/chunks/app/layout-e0a3d4e74496e50c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4882:function(n,e,u){Promise.resolve().then(u.t.bind(u,4384,23))},4384:function(){}},function(n){n.O(0,[548,971,23,744],function(){return n(n.s=4882)}),_N_E=n.O()}]);
